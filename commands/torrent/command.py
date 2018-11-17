70f4908ed40d41ef2e97506a89f2ea16828a905c
federulli <fede.rulli@hotmail.com>
2018-11-16 22:33:54 -0300

    fix list torrent

--- commands/torrent/command.py
+++ commands/torrent/command.py
@@ -1,12 +1,11 @@
 import requests
-from ..utils import get_host
 
 
 def list_torrents(bot, update):
-    r = requests.get('{}/query/torrents'.format(get_host()))
+    r = requests.get('http://127.0.0.1:8080/query/torrents')
     bot.send_message(
         chat_id=update.message.chat_id,
-        text='\n'.join("{} {:.0f}% {}".format(
+        text='\n'.join("{} {:.2%} {}".format(
             torrent['name'],
             torrent['progress'],
             torrent['state']
