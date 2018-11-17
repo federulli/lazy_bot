8dca08e0d56fc37d63ebe624e3c59bac15551824
federulli <fede.rulli@hotmail.com>
2018-11-17 12:20:07 -0300

    supervisor conf

--- commands/torrent/command.py
+++ commands/torrent/command.py
@@ -5,7 +5,7 @@ def list_torrents(bot, update):
     r = requests.get('http://127.0.0.1:8080/query/torrents')
     bot.send_message(
         chat_id=update.message.chat_id,
-        text='\n'.join("{} {:.2%} {}".format(
+        text='\n\n'.join("{}\n{:.2%}\n{}".format(
             torrent['name'],
             torrent['progress'],
             torrent['state']
